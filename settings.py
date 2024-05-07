5d0cb5edcc2a3539d26c3a84c14c405e94d27642
fahad-folio3 <169074108+fahad-folio3@users.noreply.github.com>
2024-05-08 00:29:21 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -28,10 +28,10 @@ DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'office_4_detected.jpg'
 # Videos config
 VIDEO_DIR = ROOT / 'videos'
 VIDEOS_DICT = {
-    'video_1': VIDEO_DIR / 'video_1.mp4',
+    'video_1': VIDEO_DIR / 'hardhat.mp4',
     'video_2': VIDEO_DIR / 'video_2.mp4',
     'video_3': VIDEO_DIR / 'video_3.mp4',
-    'video_4': VIDEO_DIR / 'hardhat.mp4',
+    'video_4': VIDEO_DIR / 'video_1.mp4',
 }
 
 # ML Model config
